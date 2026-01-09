2849fc4c355631df1432b1108a16c150bf8d0d91
oneffive <egorova23y@gmail.com>
2026-01-09 18:00:59 +0300

    import views in urls.py and fix NameError

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('events.urls')), 
-    path('delete/<int:sub_id>/', views.delete_subscription, name='delete_subscription'), 
+    path('', include('events.urls')),
 ]
